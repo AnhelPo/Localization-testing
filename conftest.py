44b5b6c02a7db8e0d1e8df50da607a5cae191b72
AnhelPo <100672280+AnhelPo@users.noreply.github.com>
2022-03-16 20:46:24 +0300

    Update conftest.py
    Put browser options inside the fixture to be clearer.  
    Earlier options were called by the separate method.

--- conftest.py
+++ conftest.py
@@ -18,28 +18,24 @@ def pytest_addoption(parser):
                      help='Choose language: ru, en, es...')
 
 
-def browser_options(browser_name, user_language):
-    if browser_name == 'chrome':
-        chrome_options = Options()
-        chrome_options.add_experimental_option('prefs',
-                {'intl.accept_languages': user_language})
-        driver = webdriver.Chrome(options=chrome_options)
-
-    elif browser_name == 'firefox':
-        firefox_options = webdriver.FirefoxProfile()
-        firefox_options.set_preference(
-                'intl.accept_languages', user_language)
-        driver = webdriver.Firefox(firefox_profile=firefox_options)
-    return driver
-
-
 @pytest.fixture(scope="function")
 def browser(request):
     browser_name = request.config.getoption('browser_name')
     user_language = request.config.getoption('language')
 
     if browser_name in SUPPORTED_BROWSERS:
-        driver = browser_options(browser_name, user_language)
+        if browser_name == 'chrome':
+            chrome_options = Options()
+            chrome_options.add_experimental_option('prefs',
+                        {'intl.accept_languages': user_language})
+            driver = webdriver.Chrome(options=chrome_options)
+
+        elif browser_name == 'firefox':
+            firefox_options = webdriver.FirefoxProfile()
+            firefox_options.set_preference(
+                        'intl.accept_languages', user_language)
+            driver = webdriver.Firefox(firefox_profile=firefox_options)
+            
     else:
         supported = ', '.join(SUPPORTED_BROWSERS)
         raise pytest.UsageError(f'Supported browsers: {supported}')
